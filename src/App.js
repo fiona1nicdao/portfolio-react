22467380e9490e968b2cbc255109134e17ca2624
FionaN <fiona1941nicdao@gmail.com>
2022-02-22 21:04:51 -0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,15 @@
 import './assets/css/reset.css'
 import './assets/css/App.css'
-import Aboutme from './components/aboutme/Aboutme'
+// import {NavLink, Routes, Route} from 'react-router-dom'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-      <h1 className="name">Fiona Nicdao</h1>
-        <nav className="col-md-6">
-            <ul>
-                <li>
-                    <a href="#About-Me">About Me</a>
-                </li>
-                <li>
-                    <a href="#Work">Work</a>
-                </li>
-                <li>
-                    <a href="#Contact-Me">Contact Me</a>
-                </li>
-                <li>
-                    <a href="#Resume">Resume</a>
-                </li>
-            </ul>
-        </nav>
-      </header>
-      <main>
-        <h1>Hello</h1>
-        <Aboutme/>
-      </main>
-      <footer id="Contact-Me">
-      <h3 className="Me">
-            Contact Me
-        </h3>
-        <ul className="contact">
-            <li>
-                <a href="https://www.linkedin.com/in/fiona-nicdao221513175luc/">
-                    LinkedIn
-                </a>
-            </li>
-            <li>
-                <a href="https://github.com/fiona1nicdao">
-                GitHub
-                </a>
-            </li>
-            <li>
-                <a href="https://stackoverflow.com/users/story/16901383?newreg=75f0e74564eb459091c31304de659394&_=1">
-                Stack Overflow
-                </a>
-            </li>
-            <li id="Resume">
-                <a href="https://drive.google.com/file/d/1TqaHmgJwhzWoQlenXV5DLSzH1KAmOZvA/preview">Resume</a>
-            </li>
-            <li className="email">fiona.nicdao@northwestern.edu</li>
-        </ul>
-      </footer>
+        <Header/>
+        <Footer/>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,45 @@
+import {NavLink, Routes, Route} from 'react-router-dom'
+import Aboutme from '../aboutme/Aboutme'
+
+import Contact from '../contact/Contact'
+import Work from '../work/Work'
+
+
 export default function Header(){
     return(
-        <header>
-        <h1 className="name">Fiona Nicdao</h1>
-          <nav className="col-md-6">
-              <ul>
-                  <li>
-                      <a href="#About-Me">About Me</a>
-                  </li>
-                  <li>
-                      <a href="#Work">Work</a>
-                  </li>
-                  <li>
-                      <a href="#Contact-Me">Contact Me</a>
-                  </li>
-                  <li>
-                      <a href="#Resume">Resume</a>
-                  </li>
-              </ul>
-          </nav>
-        </header>
+        <div>
+            <header>
+            <h1 className="name">Fiona Nicdao</h1>
+                <nav className="col-md-6">
+                    <ul>
+                        <li>
+                                <NavLink to='/'>About Me</NavLink>
+                            {/* <a href="#About-Me">About Me</a> */}
+                        </li>
+                        <li>
+                                <NavLink to='/work'>Work</NavLink>
+                            {/* <a href="#Work">Work</a> */}
+                        </li>
+                        <li>
+                                <NavLink to='/contact'>Contact Me</NavLink>
+                            {/* <a href="#Contact-Me">Contact Me</a> */}
+                        </li>
+                        <li>
+                            <NavLink to='/resume'>Resume</NavLink>
+                            {/* <a href="#Resume">Resume</a> */}
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Routes>
+                    <Route path='/'element={<Aboutme/>}/>
+                    <Route path='/work'element={<Work/>}/>
+                    <Route path='/contact'element={<Contact/>}/>
+                </Routes> 
+            </main>
+        </div>
+
 
     )
 }
