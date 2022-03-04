993660681b6f004533d72eaf378e2cc9a694d0d7
FionaN <fiona1941nicdao@gmail.com>
2022-03-03 23:06:49 -0600

    add HashRouter and gh-pages

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+    {/* <BrowserRouter> */}
+    <App />
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
