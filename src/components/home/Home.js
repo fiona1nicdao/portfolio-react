6532051e74770998dc3d843a389a2678e25a4255
FionaN <fiona1941nicdao@gmail.com>
2022-03-03 21:23:01 -0600

    fix cards

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ export default function Home(){
     return(
         <div >
             <h1 className="horizons">Coding to New Horizons </h1>
+            <h1 className='water'>A Web Developer with A Fresh Prespective</h1>
         </div>
     )
 }
--- src/components/work/Card.js
+++ src/components/work/Card.js
@@ -9,81 +9,93 @@ import food from '../../assets/images/foodweather.png'
 import social from '../../assets/images/sunrise.png'
 
 
-export default function Card({item}){
-    // const {id, title, app, github, proficiency, img} = item
+export default function Card(){
 
     return(
-        // <div className="manycards">
-            <div className='cards'>
-                <img className='image' alt={item.title}src={image}/>
-                <div className='overlay'>
-                    <div className='textCard'>
-                        <a className='paddingright' href={item.app}>{item.title}</a>
-                        <a href={item.github}>
-                            <FontAwesomeIcon icon={faGithub}/>
-                        </a>
+        <>
+            <div className='manycards'>
+                <div className='cards'>
+                    <img className='image' alt="ProBonoPals"src={probros}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://pro-bono-pals.herokuapp.com/">ProBonoPals</a>
+                            <a href="https://github.com/pro-bono-pals/pro-bono-pals">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>Model View Controller</p>
                     </div>
-                    <p className='proficiency'>{item.proficiency}</p>
+                </div>
+
+                <div className='cards'>
+                    <img className='image' alt="Weather Website"src={weather}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://fiona1nicdao.github.io/Homework_06/">Weather Website</a>
+                            <a href="https://github.com/fiona1nicdao/Homework_06">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>Server Side APIs</p>
+                    </div>
+                </div>
+
+                <div className='cards'>
+                    <img className='image' alt="What's your Food Temp"src={food}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://seasons-of-food.github.io/whats-your-food-temp/">What's your Food Temp</a>
+                            <a href="https://github.com/seasons-of-food/whats-your-food-temp">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>Third-Party APIs</p>
+                    </div>
                 </div>
             </div>
-        // </div>
+
+            <div className='manycards'>
+                <div className='cards'>
+                    <img className='image' alt="Quiz Website"src={quiz}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://fiona1nicdao.github.io/Homework_04/">Quiz Website</a>
+                            <a href="https://github.com/fiona1nicdao/Homework_04">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>Web APIs</p>
+                    </div>
+                </div>
+
+                <div className='cards'>
+                    <img className='image' alt="Tech Blogs"src={tech}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://tech-blog-fionanicdao.herokuapp.com/">Tech Blogs</a>
+                            <a href="https://github.com/fiona1nicdao/Tech_Blog_MVC">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>SQL</p>
+                    </div>
+                </div>
+
+                <div className='cards'>
+                    <img className='image' alt="Social Network API"src={social}/>
+                    <div className='overlay'>
+                        <div className='textCard'>
+                            <a className='paddingright' href="https://github.com/fiona1nicdao/social_network_api">Social Network API</a>
+                            <a href="https://github.com/fiona1nicdao/social_network_api">
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
+                        <p className='proficiency'>MongoDB</p>
+                    </div>
+                </div>
+            </div>
+        </>
 
     )
 }
-function Redo(){
-    return(
-        <div>
-            <div className='cards'>
-                <img className='image' alt="ProBonoPals"src={probros}/>
-                <div className='overlay'>
-                    <div className='textCard'>
-                        <a className='paddingright' href="https://pro-bono-pals.herokuapp.com/">ProBonoPals</a>
-                        <a href="https://github.com/pro-bono-pals/pro-bono-pals">
-                            <FontAwesomeIcon icon={faGithub}/>
-                        </a>
-                    </div>
-                    <p className='proficiency'>Model View Controller</p>
-                </div>
-            </div>
 
-            <div className='cards'>
-                <img className='image' alt="Weather Website"src={weather}/>
-                <div className='overlay'>
-                    <div className='textCard'>
-                        <a className='paddingright' href="https://fiona1nicdao.github.io/Homework_06/">Weather Website</a>
-                        <a href="https://github.com/fiona1nicdao/Homework_06">
-                            <FontAwesomeIcon icon={faGithub}/>
-                        </a>
-                    </div>
-                    <p className='proficiency'>Server Side APIs</p>
-                </div>
-            </div>
-
-            <div className='cards'>
-                <img className='image' alt="Quiz Website"src={quiz}/>
-                <div className='overlay'>
-                    <div className='textCard'>
-                        <a className='paddingright' href=""></a>
-                        <a href="">
-                            <FontAwesomeIcon icon={faGithub}/>
-                        </a>
-                    </div>
-                    <p className='proficiency'></p>
-                </div>
-            </div>
-
-            <div className='cards'>
-                <img className='image' alt=""src={}/>
-                <div className='overlay'>
-                    <div className='textCard'>
-                        <a className='paddingright' href=""></a>
-                        <a href="">
-                            <FontAwesomeIcon icon={faGithub}/>
-                        </a>
-                    </div>
-                    <p className='proficiency'></p>
-                </div>
-            </div>
-        </div>
-    )
-}
