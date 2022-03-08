1e91bb539bf1b993f673c792f9f313c8da2deaa8
Fiona Nicdao <fiona1941nicdao@gmail.com>
2022-03-08 15:48:52 -0600

    change resume link

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
         <div className="resume background">
             <h1 className="resume-name">Resume</h1>
             <section className="resume-section">
-                <h3 className="spacing">Download my <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1TqaHmgJwhzWoQlenXV5DLSzH1KAmOZvA/preview">Resume</a> </h3>
+                <h3 className="spacing">Download my <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/u/0/folders/1YtW9TudY6MJt8B6x3GK1YEEF1RJN6EwE">Resume</a> </h3>
                 <h3 className="spacing">Email: fiona.nicdao@northwestern.edu</h3>
                 <div className="proficiency">
                     <div>
